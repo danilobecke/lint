df0c3b56dc91490805aea9d5c27ce90f180d5cff
Danilo Becke <danilo.becke@eldorado.org.br>
2017-06-21 14:53:43 -0300

    now, checks if there is files to lint

--- lintDiffOnly.sh
+++ lintDiffOnly.sh
@@ -2,6 +2,11 @@ mkdir to-lint
 git status -s | cut -b4- | egrep '\.swift' | while read filename; do
 	cp $filename to-lint 
 done
-swiftlint lint --path to-lint --reporter "html" > out.html
+
+if find to-lint -mindepth 1 -print -quit | grep -q .; then
+	swiftlint lint --path to-lint --reporter "html" > out.html
+	open out.html
+else
+	echo "Lint didn't find any file"
+fi
 rm -rf to-lint
-open out.html
