a6b70500edb0a9299659c94e74e782ffaa227e79
Danilo Becke <danilo.becke@eldorado.org.br>
2017-06-23 18:56:30 -0300

    rules to be excluded should be written after the -i argument. Needs to fix the error output when there isn't .swiftlint.yml file. Needs to check when there aren't files to analyze.

--- lintDiffOnly.sh
+++ lintDiffOnly.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+while getopts i: option; do
+	case "$option" in
+		i) RULES+=("$OPTARG");;
+	esac
+done
+shift $((OPTIND -1))
+if [ -n "$RULES" ]; then
+	printf "disabled_rules:\n" > .swiftlint.yml
+	for rule in "${RULES[@]}"; do
+		TO_EXCLUDE+="- ${rule}"$'\n'
+	done
+	echo "${TO_EXCLUDE}" >> .swiftlint.yml
+fi
+
 mkdir to-lint
 git status -s | grep -v " D" | cut -b4- | egrep '\.swift' | while read filename; do
 	cp $filename to-lint 
@@ -9,4 +25,5 @@ if find to-lint -mindepth 1 -print -quit | grep -q .; then
 else
 	echo "Lint didn't find any file"
 fi
+rm .swiftlint.yml
 rm -rf to-lint
